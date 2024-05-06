e4f3c0b9bd9e1995d9a995018ad6d6f6e5c83c12
DLJ7666 <jarguezduran@gmail.com>
2024-05-06 13:19:09 +0200

    renombrado de VimeoMinerApplicationTests

--- src/test/java/aiss/vimeominer/VimeoMinerApplicationTests.java
+++ src/test/java/aiss/vimeominer/VimeoMinerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VimeominerApplicationTests {
+class VimeoMinerApplicationTests {
 
 	@Test
 	void contextLoads() {
