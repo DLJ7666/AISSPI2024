46575d45943ed14ce4d34f93af235e20e83eae11
DLJ7666 <jarguezduran@gmail.com>
2024-05-06 13:15:12 +0200

    creacion de VimeoMinerApplication

---
+++ src/main/java/aiss/vimeominer/VimeoMinerApplication.java
@@ -0,0 +1,14 @@
+package aiss.vimeominer;
+
+import aiss.videominer.VideominerApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VimeoMinerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VimeoMinerApplication.class, args);
+    }
+
+}
