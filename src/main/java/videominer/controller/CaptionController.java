b4ce895d1621366d67bf8bee263f2d329278a714
DLJ7666 <jarguezduran@gmail.com>
2024-05-06 22:11:36 +0200

    CaptionController 2.0

--- src/main/java/videominer/controller/CaptionController.java
+++ src/main/java/videominer/controller/CaptionController.java
@@ -1,40 +1,112 @@
 package videominer.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import videominer.exceptions.CaptionNotFoundException;
+import videominer.exceptions.ChannelNotFoundException;
+import videominer.exceptions.VideoNotFoundException;
 import videominer.model.Caption;
+import videominer.model.Channel;
+import videominer.model.Video;
 import videominer.repository.CaptionRepository;
+import videominer.repository.ChannelRepository;
 import videominer.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/videominer/captions")
+@RequestMapping("/api/videominer/channels")
 public class CaptionController {
 
     @Autowired
-    CaptionRepository captionRepository;
+    ChannelRepository channelRepository;
 
     @Autowired
     VideoRepository videoRepository;
 
-    //Obtener un subtítulo con su id
-    //GET http://localhost:8080/videominer/captions/:id
-    @GetMapping("/{id}")
-    public Caption findOne(@PathVariable Long id) throws CaptionNotFoundException {
-        Optional<Caption> caption = captionRepository.findById(id);
-        if (caption.isEmpty()) {throw new CaptionNotFoundException(); }
-        return caption.get();
+    @Autowired
+    CaptionRepository captionRepository;
+
+    @GetMapping("/{channelId}/videos/{videoId}/captions/{captionId}")
+    public Caption readCaption(@PathVariable Long channelId, @PathVariable Long videoId, @PathVariable Long captionId)
+            throws CaptionNotFoundException, ChannelNotFoundException, VideoNotFoundException {
+        Caption res = null;
+        Optional<Channel> channel = channelRepository.findById(channelId);
+        if (channel.isPresent()) {
+            Optional<Video> video = videoRepository.findById(videoId);
+            if (video.isPresent()) {
+                Optional<Caption> caption = captionRepository.findById(captionId);
+                if (caption.isEmpty()) {throw new CaptionNotFoundException(); }
+                res = caption.get();
+            } else { throw new VideoNotFoundException(); }
+        } else { throw new ChannelNotFoundException(); }
+        return res;
     }
 
-    //Obtener todos los subtítulos
-    //GET http://localhost:8080/videominer/captions
+    @GetMapping("/{channelId}/videos/{videoId}/captions")
+    public List<Caption> readCaptions(@PathVariable Long channelId, @PathVariable Long videoId)
+            throws ChannelNotFoundException, VideoNotFoundException {
+        List<Caption> res = null;
+        Optional<Channel> channel = channelRepository.findById(channelId);
+        if (channel.isPresent()) {
+            Optional<Video> video = videoRepository.findById(videoId);
+            if (video.isPresent()) {
+                res = video.get().getCaptions();
+            } else { throw new VideoNotFoundException(); }
+        } else { throw new ChannelNotFoundException(); }
+        return res;
+    }
 
-    @GetMapping
-    public List<Caption> findAll(){return captionRepository.findAll();}
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{channelId}/videos/{videoId}/captions")
+    public Caption createCaption(@PathVariable Long channelId, @PathVariable Long videoId,
+                               @Valid @RequestBody Caption caption) throws ChannelNotFoundException,
+            VideoNotFoundException {
+        Optional<Channel> channel = channelRepository.findById(channelId);
+        if (channel.isPresent()) {
+            Optional<Video> video = videoRepository.findById(videoId);
+            if (video.isPresent()) {
+                video.get().addCaption(caption);
+                return captionRepository.save(caption);
+            } else { throw new VideoNotFoundException(); }
+        } else { throw new ChannelNotFoundException(); }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{channelId}/videos/{videoId}/captions/{captionId}")
+    public void deleteCaption(@PathVariable Long channelId, @PathVariable Long videoId, @PathVariable Long captionId)
+            throws ChannelNotFoundException, VideoNotFoundException, CaptionNotFoundException {
+        Optional<Channel> channel = channelRepository.findById(channelId);
+        if (channel.isPresent()) {
+            Optional<Video> video = videoRepository.findById(videoId);
+            if (video.isPresent()) {
+                if (captionRepository.existsById(captionId)){
+                    captionRepository.deleteById(captionId);
+                } else {throw new CaptionNotFoundException(); }
+            } else { throw new VideoNotFoundException(); }
+        } else { throw new ChannelNotFoundException(); }
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{channelId}/videos/{videoId}/captions/{captionId}")
+    public void updateCaption(@PathVariable Long channelId, @PathVariable Long videoId, @PathVariable Long captionId,
+                              @Valid @RequestBody Caption updatedCaption)
+            throws ChannelNotFoundException, VideoNotFoundException, CaptionNotFoundException {
+        Optional<Channel> channel = channelRepository.findById(channelId);
+        if (channel.isPresent()) {
+            Optional<Video> video = videoRepository.findById(videoId);
+            if (video.isPresent()) {
+                Optional<Caption> toUpdateCaption =captionRepository.findById(captionId);
+                if (toUpdateCaption.isPresent()){
+                    Caption updatingCaption = toUpdateCaption.get();
+                    updatingCaption.setName(updatedCaption.getName());
+                    updatingCaption.setLanguage(updatedCaption.getLanguage());
+                } else {throw new CaptionNotFoundException(); }
+            } else { throw new VideoNotFoundException(); }
+        } else { throw new ChannelNotFoundException(); }
+    }
+
 }
