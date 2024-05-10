f5c6925db89a37084e94bf0bd56533d44a6c12ad
DLJ7666 <jarguezduran@gmail.com>
2024-05-10 23:59:14 +0200

    cambiada la uri del UserController

--- src/main/java/videominer/controller/UserController.java
+++ src/main/java/videominer/controller/UserController.java
@@ -20,7 +20,7 @@ import videominer.repository.VideoRepository;
 import java.util.List;
 import java.util.Optional;
 
-/// Uri: http://localhost:8080/api/videominer/channels/{channelId}/videos/{videoId}/comments/{commentId}/users
+/// Uri: http://localhost:8080/api/videominer/users
 @RestController
 @RequestMapping("/api/videominer/channels")
 public class UserController {
@@ -37,85 +37,43 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("/{channelId}/videos/{videoId}/comments/{commentId}/users/{userId}")
-    public User readUser(@PathVariable Long channelId, @PathVariable Long videoId, @PathVariable Long commentId,
-                            @PathVariable Long userId) throws CommentNotFoundException, ChannelNotFoundException,
-                                VideoNotFoundException, UserNotFoundException {
+    @GetMapping("/{userId}")
+    public User readUser(@PathVariable Long userId) throws UserNotFoundException {
         User res = null;
-        Optional<Channel> channel = channelRepository.findById(channelId);
-        if (channel.isPresent()) {
-            Optional<Video> video = videoRepository.findById(videoId);
-            if (video.isPresent()) {
-                Optional<Comment> comment = commentRepository.findById(commentId);
-                if(comment.isPresent()){
-                    Optional<User> user = userRepository.findById(userId);
-                    if (user.isEmpty()) {throw new UserNotFoundException(); }
-                    res = user.get();
-                } else { throw new CommentNotFoundException(); }
-            } else { throw new VideoNotFoundException(); }
-        } else { throw new ChannelNotFoundException(); }
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isEmpty()) {throw new UserNotFoundException(); }
+        res = user.get();
         return res;
     }
 
-    //Como solo hay un usuario por comentario, no tiene sentido hacer una lista de usuarios que escribieron el comentario con id commentid
+    @GetMapping
+    public List<User> readUsers() {
+        return userRepository.findAll();
+    }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/{channelId}/videos/{videoId}/comments/{commentId}/users/")
-    public User createUser(@PathVariable Long channelId, @PathVariable Long videoId, @PathVariable Long commentId,
-                                 @Valid @RequestBody User user) throws ChannelNotFoundException,
-                                    VideoNotFoundException, CommentNotFoundException {
-        Optional<Channel> channel = channelRepository.findById(channelId);
-        if (channel.isPresent()) {
-            Optional<Video> video = videoRepository.findById(videoId);
-            if (video.isPresent()) {
-                Optional<Comment> comment = commentRepository.findById(commentId);
-                if (comment.isPresent()) {
-                return userRepository.save(user);
-                } else { throw new CommentNotFoundException(); }
-            } else { throw new VideoNotFoundException(); }
-        } else { throw new ChannelNotFoundException(); }
+    @PostMapping
+    public User createUser(@Valid @RequestBody User user) {
+        return userRepository.save(user);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{channelId}/videos/{videoId}/comments/{commentId}/users/{userId}")
-    public void deleteUser(@PathVariable Long channelId, @PathVariable Long videoId, @PathVariable Long commentId,@PathVariable Long userId)
-            throws ChannelNotFoundException, VideoNotFoundException, CommentNotFoundException,UserNotFoundException {
-        Optional<Channel> channel = channelRepository.findById(channelId);
-        if (channel.isPresent()) {
-            Optional<Video> video = videoRepository.findById(videoId);
-            if (video.isPresent()) {
-                Optional<Comment> comment = commentRepository.findById(commentId);
-                if (comment.isPresent()) {
-                    if (userRepository.existsById(userId)){
-                        userRepository.deleteById(userId);
-                    } else {throw new UserNotFoundException(); }
-                } else { throw new CommentNotFoundException(); }
-            } else { throw new VideoNotFoundException(); }
-        } else { throw new ChannelNotFoundException(); }
+    @DeleteMapping("/{userId}")
+    public void deleteUser(@PathVariable Long userId) throws UserNotFoundException {
+        if (userRepository.existsById(userId)) { userRepository.deleteById(userId); }
+        else {throw new UserNotFoundException(); }
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PutMapping("/{channelId}/videos/{videoId}/captions/{commentId}/users/{userId}")
-    public void updateUser(@PathVariable Long channelId, @PathVariable Long videoId, @PathVariable Long commentId,@PathVariable Long userId,
-                              @Valid @RequestBody User updatedUser)
-            throws ChannelNotFoundException, VideoNotFoundException, CommentNotFoundException, UserNotFoundException {
-        Optional<Channel> channel = channelRepository.findById(channelId);
-        if (channel.isPresent()) {
-            Optional<Video> video = videoRepository.findById(videoId);
-            if (video.isPresent()) {
-                Optional<Comment> comment =commentRepository.findById(commentId);
-                if (comment.isPresent()){
-                    Optional<User> toUpdateUser = userRepository.findById(userId);
-                    if (toUpdateUser.isPresent()){
-                    User updatingUser = toUpdateUser.get();
-                    updatingUser.setName(updatedUser.getName());
-                    updatingUser.setUser_link(updatedUser.getUser_link());
-                    updatingUser.setPicture_link(updatedUser.getPicture_link());
-                    } else {throw new UserNotFoundException(); }
-                } else {throw new CommentNotFoundException(); }
-            } else { throw new VideoNotFoundException(); }
-        } else { throw new ChannelNotFoundException(); }
+    @PutMapping("/{userId}")
+    public void updateUser(@PathVariable Long userId, @Valid @RequestBody User updatedUser) throws UserNotFoundException {
+        Optional<User> toUpdateUser = userRepository.findById(userId);
+        if (toUpdateUser.isPresent()) {
+            User updatingUser = toUpdateUser.get();
+            updatingUser.setName(updatedUser.getName());
+            updatingUser.setUser_link(updatedUser.getUser_link());
+            updatingUser.setPicture_link(updatedUser.getPicture_link());
+        } else { throw new UserNotFoundException(); }
     }
-
 }
 
